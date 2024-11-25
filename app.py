5b22a1c64cd1209161a27714b7e5836be1d7ba74
vykle <vykhanhle9971@gmail.com>
2024-11-25 00:02:08 -0600

    Add routes

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+API_KEY = "TEsUK8PqA29HlbnR7csU3nbFLsOCMQCM"
+URL = "https://calendarific.com/api/v2"
+
 @app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html')
+
+@app.route('/holidays')
+@app.route('/holidays.html')
+def index():
+    return render_template('holidays.html')
+
+@app.route('/about')
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+@app.route('/test.html')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
